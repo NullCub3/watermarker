2eb47acf26aa381cedd6137a2361e96358e0660c
NullCub3 <51034487+NullCub3@users.noreply.github.com>
2022-07-24 16:40:32 -0700

    File and File Structure Checks

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-
 import yaml
 from pathlib import Path
 from PIL import Image
+import logging
 
 with open("config.yml", "r") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
 photos_path = Path(config["photos_dir"])
+logging.basicConfig(level=config["logging_level"])
 
 
 def merge(im1, im2):
@@ -21,9 +22,24 @@ def merge(im1, im2):
 
 
 if __name__ == "__main__":
-    print(photos_path)
-    print(config)
+
+    # Check to make sure that all the folders are present in the photos directory
+    for x in config["folders"]:
+        tempPath = photos_path / config["folders"][x]
+        if tempPath.exists():
+            logging.debug(str(x) + " folder was found!")
+        else:
+            raise Exception("Filestructure Error, '" + str(x) + "' folder does not exist")
+
+    # Check to make sure all the watermarks are in the folder
+    water_path = photos_path / config["folders"]["WATERMARK"]
+    for x in config["watermarks"]:
+        for y in config["watermarks"][x]:
+            checkFileDir = water_path / config["watermarks"][x][y]
+            if checkFileDir.is_file():
+                logging.debug(str(y) + " watermark file was found!")
+            else:
+                raise Exception("File Error, '" + str(y) + "' watermark does not exist")
 
     # Loop program:
-    while True:
-        exit()
+    # while True:
