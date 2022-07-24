8e2294657347af1cb0ab6c685481d30b3705a1bd
NullCub3 <51034487+NullCub3@users.noreply.github.com>
2022-07-23 17:23:41 -0700

    pillow testing

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 
 import yaml
 import pathlib
+from PIL import Image
 
 with open("config.yml", "r") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
-print(config["photos_dir"])
+
+def merge(im1, im2):
+    w = im1.size[0]
+    h = im1.size[1]
+    im = Image.new("RGBA", (w, h))
+
+    im.paste(im1)
+    im.paste(im2, None, im2)
+
+    return im
+
+
+image1 = Image.open('images/image1.jpg')
+image2 = Image.open('images/image2.png')
+
+result = merge(image1, image2)
+result.save('imageresult')
+
